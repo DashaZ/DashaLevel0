4cbcf66914cc2fd9397df68ea40903c2bed8f131
Ollie Pai <ollie.pai2003@gmail.com>
2016-09-25 16:02:38 -0700

    learning about the zodiac

---
+++ src/Zodiac.java
@@ -0,0 +1,42 @@
+import javax.swing.JOptionPane;
+public class Zodiac {
+public static void main(String[] args) {
+String zodiac =JOptionPane.showInputDialog("What is your zodiac sign?");
+if (zodiac.equals("taurus")) {
+JOptionPane.showMessageDialog(null,("you are stubborn, reliable, practical and stable. You enjoy music and hate sudden changes."));
+}
+if(zodiac.equals("leo")){
+JOptionPane.showMessageDialog(null, ("you work well with aries and sagittarius."));
+}
+if(zodiac.equals("scorpio")){
+JOptionPane.showMessageDialog(null, ("You like being right and hate passive people."));
+}
+if(zodiac.equals("gemini")){
+JOptionPane.showMessageDialog(null, ("you are two-faced and have everchanging emotions"));
+}
+if(zodiac.equals("aries")){
+JOptionPane.showMessageDialog(null, ("you are stubborn, passive, and don't think before you act"));
+}
+if(zodiac.equals("libra")){
+JOptionPane.showMessageDialog(null, ("you are peaceful and fair, and hate being alone"));
+}
+if(zodiac.equals("cancer")){
+JOptionPane.showMessageDialog(null, (""));
+}
+if(zodiac.equals("pisces")){
+JOptionPane.showMessageDialog(null, (""));
+}
+if(zodiac.equals("aquarius")){
+JOptionPane.showMessageDialog(null, ("you will get a flue shot today"));
+}
+if(zodiac.equals("virgo")){
+JOptionPane.showMessageDialog(null, (""));
+}
+if(zodiac.equals("sagittarius")){
+JOptionPane.showMessageDialog(null, (""));
+}
+if(zodiac.equals("capricorn")){
+JOptionPane.showMessageDialog(null, (""));
+}
+}
+}
