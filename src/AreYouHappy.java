09a195e0862554f9c5b30d2bce363506fa0f7d80
Dasha Z <league@WorthyMini-5.attlocal.net>
2017-02-12 16:01:22 -0800

    this is my google eyes start.
    void setup(){
      size(1000,1000);
    }
    void draw(){
      fill(255,255,255);
     ellipse(300,300,260,130);
     ellipse(700,300,260,130);
    fill(0,0,0);
    if(mouseX>3){
     ellipse(300,325,50,50);
    }
    ellipse(mouseX + 10,325,50,50);
    ellipse(mouseX,mouseY,50,50);
    
    }

--- src/AreYouHappy.java
+++ src/AreYouHappy.java
@@ -3,16 +3,18 @@ import java.net.URI;
 import javax.swing.JOptionPane;
 
 public class AreYouHappy {
+	static int YES = 0;
+	static int NO = 1;
 	public static void main(String[] args) {
 		boolean keepDoing = false;
 		int happy =  JOptionPane.showConfirmDialog(null, "Are You Happy?");
 			if(happy == 0){
 				keepDoing = true;
-				//playVideo()
+				playVideo("http:www.youtube.com/watch?v=H0m3Lfkzcw4");
 			}
 	else {
 			int want = JOptionPane.showConfirmDialog(null, "Do you want to be happy?");
-
+//this is fun
 			if (want == 1) {
 				keepDoing=true;
 			}
