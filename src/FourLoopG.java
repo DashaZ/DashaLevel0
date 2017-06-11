eaffee697776c9f0f49cb28c5f6ab45e033f54bd
Ryguy-1 <league@WTS10.attlocal.net>
2017-06-11 16:06:47 -0700

    c
    Xv

---
+++ src/FourLoopG.java
@@ -0,0 +1,20 @@
+
+public class FourLoopG {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 8; i+=3) {
+			for (int j = 2; j < 9; j+=3) {
+				for (int k = 3; k < 10; k+=3) {
+
+					System.out.print(i);
+				
+				System.out.print(j);
+			
+			System.out.print(k);
+			System.out.println();
+		}
+	}
+		}
+	}
+}
