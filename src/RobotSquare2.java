5949b474fcf27b112a6cb3ac1250525594f6fb39
codeavenger1 <league@WTS10.attlocal.net>
2017-09-17 16:59:54 -0700

    Fun Stuff!

---
+++ src/RobotSquare2.java
@@ -0,0 +1,21 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotSquare2 {
+	public static void main(String[] args) throws Exception {
+	// 1. Make a new Robot
+Robot bob = new Robot();
+	// 3. Put the robot's pen down
+bob.penDown();
+	// 6. Make the robot move as fast as possible
+bob.setSpeed(20);
+	// 5. Do everything below here 4 times
+for(int i =4; i>0; i--) {
+	
+	// 		2. Move your robot 200 pixels
+bob.move(200);
+	// 		4. Turn the robot 90 degrees to the right (90 degrees)
+bob.turn(90);
+	}
+}
+}
+
